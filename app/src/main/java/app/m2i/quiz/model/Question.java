a3ff1ba5ac3b851cf7416ade0e6f0de81b77508c
Sebastien Maloron <seb@maloron.com>
2017-10-31 15:10:45 +0100

    Mise en place du modèle pour la question

---
+++ app/src/main/java/app/m2i/quiz/model/Question.java
@@ -0,0 +1,51 @@
+package app.m2i.quiz.model;
+
+import java.util.UUID;
+
+public class Question {
+    private String text;
+    private UUID id;
+    private Boolean goodAnswer;
+    private Boolean userAnswer;
+
+    public Question() {
+    }
+
+    public Question(String text, Boolean goodAnswer) {
+        this.text = text;
+        this.goodAnswer = goodAnswer;
+        this.id = UUID.randomUUID();
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Boolean getGoodAnswer() {
+        return goodAnswer;
+    }
+
+    public void setGoodAnswer(Boolean goodAnswer) {
+        this.goodAnswer = goodAnswer;
+    }
+
+    public Boolean getUserAnswer() {
+        return userAnswer;
+    }
+
+    public void setUserAnswer(Boolean userAnswer) {
+        this.userAnswer = userAnswer;
+    }
+}

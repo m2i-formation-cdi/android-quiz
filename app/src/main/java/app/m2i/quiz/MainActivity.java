13239091626e61f21a5f41c66e9be8af1b708d4e
Sebastien Maloron <seb@maloron.com>
2017-11-02 13:31:05 +0100

    Gesdtion du cycle de vie de l'application

--- app/src/main/java/app/m2i/quiz/MainActivity.java
+++ app/src/main/java/app/m2i/quiz/MainActivity.java
@@ -2,6 +2,7 @@ package app.m2i.quiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,6 +14,9 @@ import app.m2i.quiz.model.Question;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
+    //Constante de classe utilisée dans les logs
+    private static final String TAG = "MainActivity";
+
     private Question currentQuestion;
     private int currentQuestionIndex = 0;
 
@@ -21,6 +25,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private Button buttonTrue;
     private Button buttonFalse;
     private List<Question> questionList;
+
+    private Button prevButton;
+    private Button nextButton;
+    private TextView navTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,27 +43,43 @@ public class MainActivity extends Activity implements View.OnClickListener {
         buttonFalse = (Button) findViewById(R.id.buttonAnswerFalse);
         buttonTrue = (Button) findViewById(R.id.buttonAnswerTrue);
 
+        //Référence aux éléments de navigation
+        prevButton = (Button) findViewById(R.id.previousButton);
+        nextButton = (Button) findViewById(R.id.nextButton);
+        navTextView = (TextView) findViewById(R.id.navTextView);
+
         //Gestion des événements
         buttonFalse.setOnClickListener(this);
         buttonTrue.setOnClickListener(this);
+        nextButton.setOnClickListener(this);
+        prevButton.setOnClickListener(this);
 
         //Définition de la liste des questions
         questionList = new ArrayList<>();
         questionList.add(new Question("Linus Torwald a inventé la poudre", false));
         questionList.add(new Question("Javascript c'est comme java", false));
-        questionList.add(new Question("Python est plus viex que Java", true));
+        questionList.add(new Question("Python est plus vieux que Java", true));
         questionList.add(new Question("Android tourne sur IOS", false));
         questionList.add(new Question("Bill Gate a inventé MS-DOS", true));
 
-        //Définition de la question en cours
-        currentQuestion = questionList.get(currentQuestionIndex);
+        //Restauration de l'état sauvegardé de l'application
+        if(savedInstanceState != null){
+            currentQuestionIndex = savedInstanceState.getInt("currentQuestionIndex");
+        }
+
         //Affichage de la question
         showQuestion();
     }
 
     private void showQuestion(){
+        //Définition de la question en cours
+        currentQuestion = questionList.get(currentQuestionIndex);
         //Affichage de la question
         questionTextView.setText(currentQuestion.getText());
+
+        //Affichage du récapitulatif de la navigation
+        String navigationText = (currentQuestionIndex + 1) + " sur " + questionList.size();
+        navTextView.setText(navigationText);
     }
 
     @Override
@@ -66,6 +90,24 @@ public class MainActivity extends Activity implements View.OnClickListener {
             checkAnswer(false);
         } else if(v == buttonTrue){
             checkAnswer(true);
+        } else if(v == prevButton){
+            goToPreviousQuestion();
+        } else if (v == nextButton){
+            goToNextQuestion();
+        }
+    }
+
+    private void goToPreviousQuestion() {
+        if(currentQuestionIndex >0){
+            currentQuestionIndex --;
+            showQuestion();
+        }
+    }
+
+    private void goToNextQuestion(){
+        if(currentQuestionIndex < questionList.size()){
+            currentQuestionIndex ++;
+            showQuestion();
         }
     }
 
@@ -81,5 +123,34 @@ public class MainActivity extends Activity implements View.OnClickListener {
         TextView questionResult = (TextView) findViewById(R.id.questionResultTextView);
         questionResult.setText(message);
 
+    }
+
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    //Sauvegarde de l'état de l'application
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        //Sauvegarde de l'index en cours
+        savedInstanceState.putInt("currentQuestionIndex", currentQuestionIndex);
+
+        super.onSaveInstanceState(savedInstanceState);
     }
 }

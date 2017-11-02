4e6c05d00c29cb916b711518229078a03a0e0033
Sebastien Maloron <seb@maloron.com>
2017-11-02 11:04:55 +0100

    Interface graphique pour la gestion de questions multiples

--- app/src/main/java/app/m2i/quiz/MainActivity.java
+++ app/src/main/java/app/m2i/quiz/MainActivity.java
@@ -6,28 +6,30 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import app.m2i.quiz.model.Question;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private Question currentQuestion;
+    private int currentQuestionIndex = 0;
+
     private TextView questionTextView;
 
     private Button buttonTrue;
     private Button buttonFalse;
+    private List<Question> questionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Création de la question
-        currentQuestion = new Question("Java c'est prise de tête ?", true);
 
         //Référence au widget TextView de la question
         questionTextView = (TextView) findViewById(R.id.questionText);
-        //Affichage de la question
-        questionTextView.setText(currentQuestion.getText());
 
         //Référence aux boutons
         buttonFalse = (Button) findViewById(R.id.buttonAnswerFalse);
@@ -36,6 +38,24 @@ public class MainActivity extends Activity implements View.OnClickListener {
         //Gestion des événements
         buttonFalse.setOnClickListener(this);
         buttonTrue.setOnClickListener(this);
+
+        //Définition de la liste des questions
+        questionList = new ArrayList<>();
+        questionList.add(new Question("Linus Torwald a inventé la poudre", false));
+        questionList.add(new Question("Javascript c'est comme java", false));
+        questionList.add(new Question("Python est plus viex que Java", true));
+        questionList.add(new Question("Android tourne sur IOS", false));
+        questionList.add(new Question("Bill Gate a inventé MS-DOS", true));
+
+        //Définition de la question en cours
+        currentQuestion = questionList.get(currentQuestionIndex);
+        //Affichage de la question
+        showQuestion();
+    }
+
+    private void showQuestion(){
+        //Affichage de la question
+        questionTextView.setText(currentQuestion.getText());
     }
 
     @Override
